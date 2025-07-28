d5418e90472a480b26dd62c63e642b3b955a25fe
Barbara Nunes <barbaranunesprogrammer@outlook.com>
2025-07-28 15:57:15 -0300

    refatoração e layout

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+  document.getElementById('whatsapp-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const servico = document.getElementById('servico').value;
+    const mensagem = document.getElementById('mensagem').value;
+
+    const numero = '5564992896111'; 
+    const texto = `Olá, meu nome é ${nome}. Gostaria de saber mais sobre: ${servico}. Mensagem: ${mensagem}`;
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(texto)}`;
+
+    window.open(url, '_blank');
+  });
